f468f312fa7b224108ff525e7b90b1be519a8565
shinhyunsu01 <shine.master9687@gmail.com>
2022-11-10 14:22:33 +0900

    init

---
+++ src/Pages/Main/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = () => {
+	return (
+		<div className="bg-red-500 flex">
+			<div className="text-blue-500">dd</div>
+		</div>
+	);
+};
+
+export default Main;
